77bf86b53634ba61f8a193766614b99424283027
Ronan Tunney <rjtunney@edu.uwaterloo.ca>
2025-03-11 20:17:44 -0400

    A few more introductory words

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,8 +11,11 @@ const HomePage = () => {
           local storage to save and configure custom tunings, youtube tutorials
           and sheet music tabs across sessions without collecting any of your
           data or serving ads. To get started, select one of the items from the
-          navigation bar above. If you have encounter any issues or feature
-          requests, please reach out to me at rjtunney@uwaterloo.ca.
+          navigation bar above. If you have any issues or feature requests,
+          please reach out to me at rjtunney@uwaterloo.ca. The website is also
+          open source and can be found{" "}
+          <a href="https://github.com/R0NAND/tooner.io">here</a> for anyone
+          willing to contribute.
         </p>
       </div>
     </div>
